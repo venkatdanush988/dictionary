a9833088bbb47f195e55df5f6dda53c4fce22b98
venkatdanush988 <22hr1a3205@mtieat.org>
2024-10-17 00:02:42 -0700

    Create keyvaluepair.py

---
+++ keyvaluepair.py
@@ -0,0 +1,27 @@
+'''
+
+
+
+4) The program takes a key-value pair and adds it to the dictionary. 
+Problem Solution:
+1. Take a key-value pair from the user and store it in separate variables.
+2. Declare a dictionary and initialize it to an empty dictionary.
+3. Use the update() function to add the key-value pair to the dictionary.
+4. Print the final dictionary.
+5. Exit.
+Sample I/O:
+12
+34
+Sample o/p:
+{12: 34}
+
+
+'''
+
+key = input("Enter the key: ")
+value = input("Enter the value: ")
+
+my_dict = {}
+my_dict.update({key: value})
+
+print(my_dict)
